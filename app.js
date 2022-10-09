73a00e2c876989d4e8f6fddb2258379ca539b3eb
akhil-anand <akhilanand.ak01@gmail.com>
2022-10-09 20:01:55 +0530

    apis

--- app.js
+++ app.js
@@ -1,14 +1,57 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const app = express();
+var cors = require('cors');
+app.use(cors({origin: '*', credentials: true}));
 
-const PORT = process.env.PORT || 8080;
+
+const mongoose = require('mongoose')
+
+mongoose.connect(
+  `mongodb+srv://akhil:akhil@cluster0.o9rhqj3.mongodb.net/budget-expenses?retryWrites=true&w=majority`, 
+  {
+    useNewUrlParser: true,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+  }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+})
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://akhil:akhil@cluster0.o9rhqj3.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("budget-monitoring").collection("expenses-data");
+
+//   client.close();
+// });
+
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb+srv://akhil:akhil@cluster0.o9rhqj3.mongodb.net/budget-expenses');
+// }
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', require('./routes/data'));
-
-app.listen(PORT, ()=>{
-    console.log('Server is running at ' + PORT)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next()
 })
+
+
+app.listen(server_port, server_ip_address, function () {
+
+    console.log( "Listening on " + server_ip_address + ", port " + server_port )
+  
+  });
--- routes/data.js
+++ routes/data.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const data = [{
     name:'akhil',
     occupation:'Developer'
 }]
+
+const expensesSchema = {
+    price: Number,
+    category: String,
+    dateOfPurchase: String,
+    month: String,
+    year: String
+
+  };
+
+  const Expense = mongoose.model("Expense", expensesSchema);
 
 router.get('/getData', (req, res)=>{
     // res.send('Welcome to my web server')
@@ -16,6 +28,43 @@ router.post('/addData', (req, res)=>{
     if(name && occupation){
         data.push({ name, occupation});
         res.json({ok:true, data})
+    }
+})
+
+router.post('/addExpense', async (req, res) => {
+    const expense = new Expense({
+        price: req.body.price,
+        category: req.body.category,
+        dateOfPurchase: moment(new Date()).format('DD-MM-YYYY'),
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    });
+    try{
+        await expense.save();
+        res.send(expense)
+    } catch (error){
+        res.status(500).send(error)
+    }
+})
+
+router.get('/getExpense', async (req, res) => {
+    const expense = await Expense.find({});
+    try{
+        res.send(expense)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/getMonthlyExpense', async (req, res) => {
+    const expense = await Expense.find({
+        month: new Date().getMonth() + 1,
+        year: new Date().getFullYear()
+    })
+    try{
+        res.send(expense)
+    } catch (error) {
+        res.status(500).send(error)
     }
 })
 
